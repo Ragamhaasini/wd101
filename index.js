9a7094f426b9ebd082a66d1cad26e3fb99992273
Ragamhaasini <151642741+Ragamhaasini@users.noreply.github.com>
2023-11-21 19:57:36 +0530

    index.js

--- index.js
+++ index.js
@@ -1,124 +1,48 @@
-let form = document.getElementById("form");
-
-// localStorage.clear();
-// let Entries = [];
-
-const retriveEntries = () => {
-  let entries = localStorage.getItem("userEntry");
-
-  if (entries) {
-    entries = JSON.parse(entries);
-  } else {
-    entries = [];
-  }
-  return entries;
-};
-
-let Entries = retriveEntries();
-
-const displayEntries = () => {
-  const entries = retriveEntries();
-
-  const rows = entries
-    .map((entry) => {
-      const name = `<td class="td">${entry.name}</td>`;
-      const email = `<td class="td">${entry.email}</td>`;
-      const password = `<td class="td">${entry.password}</td>`;
-      const dob = `<td class="td">${entry.dob}</td>`;
-      const accseptConditions = `<td class="td">${entry.accseptConditions}</td>`;
-
-      const row = `<tr>${name} ${email} ${password} ${dob} ${accseptConditions}</tr>`;
-      return row;
-    })
-    .join("\n");
-
-  let tableDiv = document.getElementById("tableDiv");
-
-  // <th class="th">Name</th> inside oneMore head for name
-  tableDiv.innerHTML = `<table class="table" border="2">
-  <tr>
-    <th class="th">Name</th>
-    <th class="th">Email</th>
-    <th class="th">Password</th>
-    <th class="th">Dob</th>
-    <th class="th">Accepted terms?</th>
-  </tr>
-    ${rows}
-  </table>`;
-};
-
-// const saveUserFrom = () => {
-const saveUserFrom = (event) => {
-  event.preventDefault();
-
-  let name = document.getElementById("name").value;
-  let email = document.getElementById("email").value;
-  let password = document.getElementById("password").value;
-  let dob = document.getElementById("dob").value;
-  let accseptConditions = document.getElementById("agree").checked;
-
-  let entry_obj = {
-    name,
-    email,
-    password,
-    dob,
-    accseptConditions,
-  };
-
-  Entries.push(entry_obj);
-
-  localStorage.setItem("userEntry", JSON.stringify(Entries));
-
-  displayEntries();
-};
-
-form.addEventListener("submit", saveUserFrom);
-
-displayEntries();
-
-// Add additional validations to the date input field so that it accepts date of birth for people between ages 18 and 55 only. You'll need to figure out how to do this.
-
-function getAge(today, birthDate) {
-  // var today = new Date();
-  // var birthDate = new Date(DOB);
-
-  var age = today.getFullYear() - birthDate.getFullYear();
-  var m = today.getMonth() - birthDate.getMonth();
-  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-    age--;
-  }
-  return age;
+const form_validate=()=>{
+const name=document.getElementById("name").value;
+const email=document.getElementById("email").value;
+const password=document.getElementById("password").value;
+const dob=document.getElementById("dob").value;
+const acceptterms=document.getElementById("acceptterm").checked;
+var Entries;
+if(localStorage.getItem('user-entries')===null)
+Entries=[];
+else
+Entries=JSON.parse(localStorage.getItem('user-entries'))
+Entries.unshift({
+name:name,
+email:email,
+password:password,
+dob:dob,
+acceptterms:acceptterms
+})
+localStorage.setItem('user-entries',JSON.stringify(Entries));
+showdata()
+document.getElementById('name').value="";
+document.getElementById('email').value="";
+document.getElementById('password').value="";
+document.getElementById('dob').value="";
+document.getElementById('acceptterm').value="true";
 }
+const showdata=()=>{
+var Entries;
+if(localStorage.getItem('user-entries')===null)
+Entries=[];
+else
+Entries=JSON.parse(localStorage.getItem('user-entries'))
 
-let dateELE = document.getElementById("dob");
+var html="";
 
-dateELE.addEventListener("change", () => {
-  let [year, month, date] = document.getElementById("dob").value.split("-");
-
-  let dob = new Date(year, month, date);
-  let Today = new Date();
-
-  age = getAge(Today, dob);
-
-  dateELE.style.border = "2px solid rgba(0, 0, 0, 0.4)";
-  if (age < 18 || age > 55) {
-    dateELE.setCustomValidity("Your age is not lies between 18 and 55");
-    dateELE.style.border = "2px solid red";
-    return;
-  } else {
-    dateELE.setCustomValidity("");
-  }
+Entries.forEach((element,intex)=>{
+html += "<tr>";
+html += '<td class="border border-slate-700 p-4">' + element.name+"</td>";
+html += '<td class="border border-slate-700 p-4">' + element.email+"</td>";
+html += '<td class="border border-slate-700 p-4">' + element.password+"</td>";
+html += '<td class="border border-slate-700 p-4">' + element.dob+"</td>";
+html += '<td class="border border-slate-700 p-4">' + element.acceptterms+"</td>";
+html += "</tr>";
 });
+document.querySelector("#regTable tbody").innerHTML=html;
 
-const email = document.getElementById("email");
-
-email.addEventListener("input", () => validate(email));
-
-function validate(ele) {
-  if (ele.validity.typeMismatch) {
-    ele.setCustomValidity("The Email is not in the right format!!!");
-    ele.reportValidity();
-  } else {
-    ele.setCustomValidity("");
-  }
 }
+document.onload=showdata();  
